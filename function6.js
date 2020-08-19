b60b1b8481682969ea6cce4843f47f8c893598b1
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2020-08-19 09:22:06 -0700

    changed whole code

--- function6.js
+++ function6.js
@@ -3,8 +3,10 @@ let food = ['apple', 'orange', 'cake', 'carrots', 'pasta', 'rice', 'eggs'];
 
 let numbers = ['1', '2', '3', '4'];
 
-const addTheLast = function () {
-    console.log(food[6], numbers[3]);
+const addTheLast = function (arr1, arr2) {
+    let arr2index = arr2.length - 1;
+    arr1.push(arr2[arr2index]);
+    console.log(arr1);
 };
 
 addTheLast(food, numbers);
